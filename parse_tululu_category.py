781273547be335d47e97964c47039d68bd57a79e
maratdzhukaev09 <60516734+maratdzhukaev09@users.noreply.github.com>
2020-03-27 15:49:57 +0300

    Created new functions

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,17 +4,15 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 from itertools import groupby
 
-parser = argparse.ArgumentParser(
-    description='This parser is designed to quickly and efficiently search for data on science fiction \
-    books from tululu.org. At the same time, you can download their contents and their covers.'
-)
-parser.add_argument('--start_page', help='First page', type=int)
-parser.add_argument('--end_page', help='Last page', type=int)
-parser.add_argument('--dest_folder', help='Path to the directory with parsing results: images, books, JSON')
-parser.add_argument('--skip_imgs', help='Do not download pictures', type=bool)
-parser.add_argument('--skip_txt', help='Do not download books')
-parser.add_argument('--json_path', help='Indicate your path to the * .json file with the results')
-args = parser.parse_args()
+def write_json(library, filename):
+    with open(filename, "w", encoding='utf8') as file:
+        json.dump([], file, ensure_ascii=False)
+    with open(filename, "r") as file:
+        library_old = json.load(file)
+    with open(filename, "w", encoding='utf8') as file:
+        library_new = [el for el, _ in groupby(library_old + library)]
+        json.dump(library_new, file, ensure_ascii=False)
+
 
 def download_picture(url, filename, folder='images/'):
     filepath = os.path.join(folder, sanitize_filename(filename))
@@ -63,37 +61,17 @@ def check_url(url):
     except:
         return False
 
-
-start_page, end_page = args.start_page, args.end_page
-if end_page is None:
-    end_page = 702
-if start_page is None:
-    start_page = 1
-
-if args.json_path:
-    filename = args.json_path
-else:
-    filename = "library.json"
-
 os.makedirs("books", exist_ok=True)
 os.makedirs("images", exist_ok=True)
-if args.dest_folder:
-    print(str(str(os.path.abspath('books/')).split('books')[0]))
 
 id_list = []
 library = []
 get_description_book('http://tululu.org/b9/')
 
-for page in range(start_page, end_page):
-    url = urljoin('http://tululu.org/l55/', str(page) + '/')
+def get_page_ids(url):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    selector_list = 'table.d_book'
-    page_id_list = [urljoin('http://tululu.org/', id_.select_one('a')['href']) for id_ in soup.select(selector_list)]
-    id_list += page_id_list
-    print(page_id_list)
-
 for id_ in id_list:
     if check_url(id_):
         book = dict()
@@ -110,11 +88,68 @@ for id_ in id_list:
             book['image_src'] = download_picture(url_image, image_filename)
 
         library.append(book)
+    page_ids = [urljoin(url, id_.select_one('a')['href']) for id_ in soup.select('table.d_book')]
 
-with open(filename, "w", encoding='utf8') as file:
-    json.dump([], file, ensure_ascii=False)
-with open(filename, "r") as file:
-    library_old = json.load(file)
-with open(filename, "w", encoding='utf8') as file:
-    library_new = [el for el, _ in groupby(library_old + library)]
-    json.dump(library_new, file, ensure_ascii=False)
+    return page_ids
+
+def work_with_args(args):
+    start_page, end_page = args.start_page, args.end_page
+    if end_page is None:
+        end_page = 702
+    if start_page is None:
+        start_page = 1
+
+    if args.json_path:
+        filename = args.json_path
+    else:
+        filename = "library.json"
+
+    if args.dest_folder:
+        print(str(str(os.path.abspath('parse_tululu_category.py')).split('parse_tululu_category.py')[0]))
+
+    return start_page, end_page, filename
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='This parser is designed to quickly and efficiently search for data on science fiction \
+            books from tululu.org. At the same time, you can download their contents and their covers.'
+    )
+    parser.add_argument('--start_page', help='First page', type=int)
+    parser.add_argument('--end_page', help='Last page', type=int)
+    parser.add_argument('--dest_folder', help='Path to the directory with parsing results: images, books, JSON')
+    parser.add_argument('--skip_imgs', help='Do not download pictures', type=bool)
+    parser.add_argument('--skip_txt', help='Do not download books')
+    parser.add_argument('--json_path', help='Indicate your path to the * .json file with the results')
+    args = parser.parse_args()
+
+    return args
+
+    args = get_args()
+    start_page, end_page, filename = work_with_args(args)
+    os.makedirs("books", exist_ok=True)
+    os.makedirs("images", exist_ok=True)
+    ids = []
+    library = []
+
+    for page in range(start_page, end_page):
+        url = urljoin('http://tululu.org/l55/', f"{str(page)}/")
+        ids += get_page_ids(url)
+
+    for id_ in ids:
+        if check_url(id_):
+            book = dict()
+            book['title'], book['author'], book['comments'], book['genres'], url_txt, url_image = get_description_book(id_)
+
+            if args.skip_txt:
+                book['book_path'] = None
+            else:
+                book['book_path'] = download_txt(url_txt, f"{book['title']}.txt")
+            if args.skip_imgs:
+                book['image_src'] = None
+            else:
+                image_filename = url_image.split('/')[-1]
+                book['image_src'] = download_picture(url_image, image_filename)
+
+            library.append(book)
+
+    write_json(library, filename)
